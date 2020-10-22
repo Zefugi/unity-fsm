00f4f66ce91db345de89a02ab53d463198972036
Zefugi <trowi@live.dk>
2020-10-22 12:32:55 +0200

    Wrote and passed Ctor_SetInitialState

---
+++ FiniteStateMachine/Packages/com.zefugi.finite-state-machine/Editor/FiniteStateMachineController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Zefugi.Unity.FiniteStateMachine
+{
+    public class FiniteStateMachine : MonoBehaviour
+    {
+        public State CurrentState { get; }
+
+        public FiniteStateMachine(State initialState)
+        {
+            CurrentState = initialState;
+        }
+    }
+}
---
+++ FiniteStateMachine/Packages/com.zefugi.finite-state-machine/Tests/Editor/FiniteStateMachine_Tests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using NSubstitute;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using Zefugi.Unity.FiniteStateMachine;
+
+namespace Tests
+{
+    public class FiniteStateMachine_Tests
+    {
+        [Test]
+        public void Ctor_SetsInitialState()
+        {
+            var initState = Substitute.For<State>();
+            var fsm = new FiniteStateMachine(initState);
+
+            Assert.AreEqual(initState, fsm.CurrentState);
+        }
+    }
+}
