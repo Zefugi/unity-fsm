bc71b17b94316562eb85c7329dc1b25373a466c0
Zefugi <trowi@live.dk>
2020-10-22 12:32:30 +0200

    Created a state model

---
+++ FiniteStateMachine/Packages/com.zefugi.finite-state-machine/Editor/State.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Zefugi.Unity.FiniteStateMachine;
+
+namespace Zefugi.Unity.FiniteStateMachine
+{
+    public class State
+    {
+        public FiniteStateMachine Machine { get; internal set; }
+    }
+}
