ce763a33d917dbb335cda92e17c992da33247dd2
Zefugi <trowi@live.dk>
2020-10-22 10:42:45 +0200

    Created the test project.

---
+++ Editor/FiniteStateMachine.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Zefugi.Unity.FiniteStateMachine
+{
+    public class FiniteStateMachine : MonoBehaviour
+    {
+        public bool Untrue => false;
+
+        private void Start()
+        {
+            Debug.Log($"Untrue is {Untrue}.");
+        }
+    }
+}
