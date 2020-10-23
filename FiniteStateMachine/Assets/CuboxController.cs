af411dee73a1f095ea7071b01959fe556c1c8524
Zefugi <trowi@live.dk>
2020-10-23 11:51:48 +0200

    Started building an example scene.

---
+++ FiniteStateMachine/Assets/CuboxController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Zefugi.Unity.FiniteStateMachine;
+
+public class CuboxController : FiniteStateMachineController
+{
+    public override FiniteStateMachineSystem StateMachine { get; }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        StateMachine.Update(this);
+    }
+}
---
+++ FiniteStateMachine/Assets/CuboxIdleState.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zefugi.Unity.FiniteStateMachine;
+
+namespace Assets
+{
+    public class CuboxIdleState : State
+    {
+    }
+}
