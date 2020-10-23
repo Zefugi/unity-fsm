21981de47f7e4474ea4961d2d2f2f8def8d46842
Zefugi <trowi@live.dk>
2020-10-23 11:42:14 +0200

    Revert "Removed the controller. This will be implemented by the client."
    This reverts commit 64d663406094bf5dad570b36596ae742cf251a75.

---
+++ Editor/FiniteStateMachineController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Zefugi.Unity.FiniteStateMachine;
+
+public class FiniteStateMachineController : MonoBehaviour
+{
+    private FiniteStateMachineSystem _machine;
+
+    private void Update()
+    {
+        _machine?.Update(this);
+    }
+}
